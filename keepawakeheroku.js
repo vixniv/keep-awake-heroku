045553d1c64953e68e55dbaa4158b96338a63eef
vixniv <vixniv@gmail.com>
2022-06-26 20:58:52 +0700

    pull from db each interval

--- keepawakeheroku.js
+++ keepawakeheroku.js
@@ -1,45 +1,53 @@
 const axios = require("axios").default;
+const App = require("./models/appModel");
 
 const keepAwakeHeroku = (urls) => {
-  const timerID = setInterval(() => {
-    const timeNow = new Date();
-    let timeNowJakarta = timeNow.toLocaleString("en-US", {
-      timeZone: "Asia/Jakarta",
-      hour: "numeric",
-      hour12: false,
-    });
+  const timerID = setInterval(async () => {
+    try {
+      urls = await App.find();
 
-    urls.forEach((url) => {
-      !url.start ? (url.start = 00) : url.start;
-      !url.end ? (url.end = 24) : url.end;
+      const timeNow = new Date();
+      let timeNowJakarta = timeNow.toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta",
+        hour: "numeric",
+        hour12: false,
+      });
 
-      // handle if time range is minus, e.g. 18-06
-      if (url.end - url.start < 0) {
-        url.end += 24;
-        if (timeNowJakarta < url.start) {
-          timeNowJakarta += 24;
-          if (timeNowJakarta < url.end) {
-            timeNowJakarta -= 24;
+      urls.forEach((url) => {
+        !url.start ? (url.start = 00) : url.start;
+        !url.end ? (url.end = 24) : url.end;
+
+        // handle if time range is minus, e.g. 18-06
+        if (url.end - url.start < 0) {
+          url.end += 24;
+          if (timeNowJakarta < url.start) {
+            timeNowJakarta += 24;
+            if (timeNowJakarta < url.end) {
+              timeNowJakarta -= 24;
+            }
           }
         }
-      }
 
-      if (url.start <= timeNowJakarta && url.end > timeNowJakarta) {
-        axios
-          .get(`https://${url.app}.herokuapp.com/`)
-          .then(() => {
-            console.log(`Wake up https://${url.app}.herokuapp.com/`);
-          })
-          .catch(() => {
-            console.log(`Error waking up https://${url.app}.herokuapp.com/`);
-          });
-      } else {
-        console.log(`Still dreaming https://${url.app}.herokuapp.com/`);
-      }
-    });
+        if (url.start <= timeNowJakarta && url.end > timeNowJakarta) {
+          console.log(`Wake up https://${url.app}.herokuapp.com/`);
+          // axios
+          //   .get(`https://${url.app}.herokuapp.com/`)
+          //   .then(() => {
+          //     console.log(`Wake up https://${url.app}.herokuapp.com/`);
+          //   })
+          //   .catch(() => {
+          //     console.log(`Error waking up https://${url.app}.herokuapp.com/`);
+          //   });
+        } else {
+          console.log(`Still dreaming https://${url.app}.herokuapp.com/`);
+        }
+      });
 
-    // keepAwakeHeroku(urls);
-  }, 29 * 60000); // ping every 29 minutes - 29 * 60000
+      // keepAwakeHeroku(urls);
+    } catch (error) {
+      console.log(error);
+    }
+  }, 5000); // ping every 29 minutes - 29 * 60000
 
   return timerID;
 };
